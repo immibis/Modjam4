68aa78d75b23af9235bcff6c79b46cb83d55b4d8
immibis <immibis@gmail.com>
2014-05-15 19:09:46 +1200

    And now they spin smoothly.

--- src/immibis/modjam4/TileShaft.java
+++ src/immibis/modjam4/TileShaft.java
@@ -4,9 +4,10 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileShaft extends TileEntity {
 	public int angle; // INT_MIN to INT_MAX
+	public int angvel = 3 << 24; // angle units per tick
 	
 	@Override
 	public void updateEntity() {
-		angle += (3 << 24);
+		angle += angvel;
 	}
 }
