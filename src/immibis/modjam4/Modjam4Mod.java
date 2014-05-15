667bd15ed59ae44193abfa495da3d5762f7ddc85
immibis <immibis@gmail.com>
2014-05-15 17:14:01 +1200

    Automatic commit.

--- src/immibis/modjam4/Modjam4Mod.java
+++ src/immibis/modjam4/Modjam4Mod.java
@@ -1,16 +1,13 @@
-package com.example.examplemod;
+package immibis.modjam4;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = "immibis_modjam4", version = "0.0")
+public class Modjam4Mod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
