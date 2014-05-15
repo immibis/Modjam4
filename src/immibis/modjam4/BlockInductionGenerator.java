04b84f776d0ee38eb171de69cb86cd290fd3a0ee
immibis <immibis@gmail.com>
2014-05-15 21:45:36 +1200

    Automatic commit.

---
+++ src/immibis/modjam4/BlockInductionGenerator.java
@@ -0,0 +1,10 @@
+package immibis.modjam4;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+
+public class BlockInductionGenerator extends BlockContainer {
+	public BlockInductionGenerator() {
+		super(Material.iron);
+	}
+}
