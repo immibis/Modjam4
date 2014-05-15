29ab13334335836c077fa78fdf1af8aafcfabe2f
immibis <immibis@gmail.com>
2014-05-15 16:43:59 +1200

    Wrote autocommit script.

--- autocommit.py
+++ autocommit.py
@@ -1,11 +1,14 @@
-import time
-import subprocess
+import time, subprocess, os
 
 
-gitpath = r'C:\Program Files (x86)\Git\bin\git.exe'
+gitpath = r"C:\Program Files (x86)\Git\bin\git.exe"
+plinkpath = r"C:\Users\Alex\plink.exe"
 
 def runcmd(args):
-        proc = subprocess.Popen(args, executable=gitpath, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        si.wShowWindow = subprocess.SW_HIDE
+        proc = subprocess.Popen(args, executable=gitpath, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, startupinfo=si)
         stdout, stderr = proc.communicate()
         stdout = stdout.decode('utf-8', 'replace')
         stderr = stderr.decode('utf-8', 'replace')
@@ -14,6 +17,13 @@ def runcmd(args):
         
 
 
-runcmd("git add .")
-runcmd("git add -u")
-runcmd("git commit -m \"Automatic commit.\"")
+def commit():
+    runcmd("git add .")
+    runcmd("git add -u")
+    runcmd("git commit -m \"Automatic commit.\" --allow-empty")
+    os.environ["GIT_SSH"] = plinkpath
+    runcmd("git push origin master --force")
+
+while True:
+    time.sleep(15*60)
+    commit()
