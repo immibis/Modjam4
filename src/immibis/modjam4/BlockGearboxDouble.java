6824a136f1730262a0770a91623334343d3f5e9e
immibis <immibis@gmail.com>
2014-05-16 13:00:34 +1200

    Automatic commit.

--- src/immibis/modjam4/BlockGearboxDouble.java
+++ src/immibis/modjam4/BlockGearboxDouble.java
@@ -4,8 +4,10 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
@@ -19,6 +21,11 @@ public class BlockGearboxDouble extends BlockContainer {
 	}
 	
 	@Override
+	public IIcon getIcon(int side, int meta) {
+		return (side & 6) == (meta & 6) ? Blocks.log.getIcon(0, 0) : super.getIcon(side, meta);
+	}
+	
+	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileGearboxDouble();
 	}
@@ -27,5 +34,6 @@ public class BlockGearboxDouble extends BlockContainer {
 	public void onBlockPlacedBy(World w, int x, int y, int z, EntityLivingBase pl, ItemStack p_149689_6_) {
 		int l = MathHelper.floor_double((double)(pl.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
         int meta = l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
+        w.setBlockMetadataWithNotify(x, y, z, meta, 3);
 	}
 }
