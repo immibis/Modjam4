6a42430f5a07da490bd1b79957ad76ea72fb7158
immibis <immibis@gmail.com>
2014-05-16 12:30:22 +1200

    Automatic commit.

--- src/immibis/modjam4/BlockGearboxDouble.java
+++ src/immibis/modjam4/BlockGearboxDouble.java
@@ -3,9 +3,13 @@ package immibis.modjam4;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
+// metadata is high-speed direction
 public class BlockGearboxDouble extends BlockContainer {
 	public BlockGearboxDouble(Material m) {
 		super(m);
@@ -17,5 +21,11 @@ public class BlockGearboxDouble extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileGearboxDouble();
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World w, int x, int y, int z, EntityLivingBase pl, ItemStack p_149689_6_) {
+		int l = MathHelper.floor_double((double)(pl.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        int meta = l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
 	}
 }
--- src/immibis/modjam4/TileGearboxDouble.java
+++ src/immibis/modjam4/TileGearboxDouble.java
@@ -2,6 +2,33 @@ package immibis.modjam4;
 
 import net.minecraft.tileentity.TileEntity;
 
-public class TileGearboxDouble extends TileEntity {
+public class TileGearboxDouble extends TileEntity implements IShaft {
+	
+	public int angle;
+	public int angvel;
+
+	@Override
+	public int getAngle(int side) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAngVel(int side) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean doesShaftConnect(int side) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getMomentOfInertia(int side) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
