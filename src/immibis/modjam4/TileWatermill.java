b19e6b82eb0bc159f98a0be1004be2c084a3e01d
immibis <immibis@gmail.com>
2014-05-16 23:18:42 +1200

    Automatic commit.

--- src/immibis/modjam4/TileWatermill.java
+++ src/immibis/modjam4/TileWatermill.java
@@ -6,9 +6,14 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.fluids.BlockFluidClassic;
 
 public class TileWatermill extends TileShaft {
+	
+	private static int LL_OBSTRUCTION = -1;
+	
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
+		
+		int meta = getBlockMetadata();
 		
 		int level1, level2, level3, numfalls;
 		switch(getBlockMetadata()) {
@@ -36,16 +41,23 @@ public class TileWatermill extends TileShaft {
 		}
 		
 		
+		if(level1 == LL_OBSTRUCTION || level2 == LL_OBSTRUCTION || level3 == LL_OBSTRUCTION || numfalls < -1000 || numfalls > 1000) {
+			// water wheel is obstructed
+			shaftNode.getNetwork().angvel *= 0.7;
+			return;
+		}
+		
 		{
 			int ANGVEL_PER_LEVEL_DIFF = ShaftUtils.fromDegreesPerSecond(13);
 			int ANGVEL_PER_WATERFALL = ShaftUtils.fromDegreesPerSecond(4);
 			int targetAngvel = ANGVEL_PER_WATERFALL * numfalls;
 			if((level3 >= level2 && level2 >= level1) || (level1 >= level2 && level2 >= level3))
-				targetAngvel += ANGVEL_PER_LEVEL_DIFF * (level3 - level1);
+				targetAngvel += ANGVEL_PER_LEVEL_DIFF * (level1 - level3);
 			int diff = targetAngvel - shaftNode.getNetwork().angvel;
 			shaftNode.getNetwork().angvel += diff * 0.5;
 		}
 	}
+	
 	
 	private int getLiquidLevel(int dx, int dy, int dz) {
 		dx+=xCoord; dy+=yCoord; dz+=zCoord;
@@ -53,9 +65,12 @@ public class TileWatermill extends TileShaft {
 			return 0;
 		Block b = worldObj.getBlock(dx, dy, dz);
 		if(!(b instanceof BlockLiquid))
-			return 0;
+			if(worldObj.isAirBlock(dx, dy, dz))
+				return 0;
+			else
+				return LL_OBSTRUCTION;
 		
-		return worldObj.getBlockMetadata(dx, dy, dz) & 7;
+		return 7 - (worldObj.getBlockMetadata(dx, dy, dz) & 7);
 	}
 	
 	private int isWaterfall(int dx, int dy, int dz) {
@@ -64,7 +79,10 @@ public class TileWatermill extends TileShaft {
 			return 0;
 		Block b = worldObj.getBlock(dx, dy, dz);
 		if(!(b instanceof BlockLiquid))
-			return 0;
+			if(worldObj.isAirBlock(dx, dy, dz))
+				return 0;
+			else
+				return -10000000;
 		
 		return (worldObj.getBlockMetadata(dx, dy, dz) & 8) >> 3;
 	}
