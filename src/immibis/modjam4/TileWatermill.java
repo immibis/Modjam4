b000289f1ff930aa5f130e4bbc6059bccb6105c3
immibis <immibis@gmail.com>
2014-05-16 22:48:29 +1200

    Automatic commit.

--- src/immibis/modjam4/TileWatermill.java
+++ src/immibis/modjam4/TileWatermill.java
@@ -1,12 +1,46 @@
 package immibis.modjam4;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraftforge.fluids.BlockFluidClassic;
 
 public class TileWatermill extends TileShaft {
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
-		shaftNode.getNetwork().angvel += ShaftUtils.fromDegreesPerSecond(3);
+		
+		int level1, level2, level3;
+		switch(getBlockMetadata()) {
+		case 0: // shaft on Y axis; ??????
+		default:
+			return;
+			
+		case 2: // shaft on Z axis; water on X axis
+			level1 = getLiquidLevel(-1, -2, 0);
+			level2 = getLiquidLevel(0, -2, 0);
+			level3 = getLiquidLevel(1, -2, 0);
+			break;
+			
+		case 4: // shaft on X axis; water on Z axis
+			level1 = getLiquidLevel(0, -2, -1);
+			level2 = getLiquidLevel(0, -2, 0);
+			level3 = getLiquidLevel(0, -2, 1);
+			break;
+		}
+		
+		shaftNode.getNetwork().angvel += ShaftUtils.fromDegreesPerSecond(0.5 * (level3 - level1));
+	}
+	
+	private int getLiquidLevel(int dx, int dy, int dz) {
+		dx+=xCoord; dy+=yCoord; dz+=zCoord;
+		if(!worldObj.blockExists(dx,dy,dz))
+			return 0;
+		Block b = worldObj.getBlock(dx, dy, dz);
+		if(!(b instanceof BlockLiquid) && !(b instanceof BlockFluidClassic))
+			return 0;
+		
+		return worldObj.getBlockMetadata(dx, dy, dz) & 7;
 	}
 	
 	@Override
