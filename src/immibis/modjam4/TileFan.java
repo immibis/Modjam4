9912391a49dbb80ae4cab37a5ea60b12db63ef73
immibis <immibis@gmail.com>
2014-05-17 16:55:18 +1200

    Automatic commit.

--- src/immibis/modjam4/TileFan.java
+++ src/immibis/modjam4/TileFan.java
@@ -5,6 +5,7 @@ import java.util.List;
 import immibis.modjam4.shaftnet.ShaftNode;
 import immibis.modjam4.shaftnet.SpeedTorqueCurve;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
@@ -24,23 +25,32 @@ public class TileFan extends TileShaft implements SpeedTorqueCurve {
 	public void updateEntity() {
 		super.updateEntity();
 		
-		double angvel_dps = ShaftUtils.toDegreesPerSecond((int)shaftNode.getNetwork().angvel);
+		double angvel_dps = Math.abs(ShaftUtils.toDegreesPerSecond((int)shaftNode.getNetwork().angvel));
 		
-		int range = 7;
+		int range = (int)angvel_dps/32;
 		
 		switch(getBlockMetadata()) {
 		case 5: // -X
-			for(Entity e : (List<Entity>)worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getAABBPool().getAABB(xCoord-0.5-range, yCoord-0.5, zCoord-0.5, xCoord-0.5, yCoord+0.5, zCoord+0.5))) {
-				e.motionX = -0.05;
-				e.posZ = (e.posZ * 0.9) + (zCoord + 0.5) * 0.1;
-				e.posZ = zCoord + 0.5;
-				e.posY = yCoord + 0.5;
-				e.lastTickPosY = e.posY;
-				e.prevPosY = e.posY;
-				e.motionY = 0.0001;
-				e.prevPosZ = e.lastTickPosZ = e.posZ;
+			for(Entity e : (List<Entity>)worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getAABBPool().getAABB(xCoord-0.5-range, yCoord-0.5, zCoord-0.5, xCoord-0.5, yCoord+1.5, zCoord+1.5))) {
+				if(e instanceof EntityPlayer)
+					continue;
+				e.motionX = e.motionX*0.8 - 0.05;
+				e.motionY += 0.04;
+				e.motionZ *= 0.8;
+				e.fallDistance = 0;
+				e.setPosition(e.posX, e.posY+(yCoord+0.5-e.posY)*0.1, e.posZ+(zCoord+0.5-e.posZ)*0.1);
 			}
 			break;
+		case 1: // -Y
+			for(Entity e : (List<Entity>)worldObj.getEntitiesWithinAABB(Entity.class, AxisAlignedBB.getAABBPool().getAABB(xCoord-0.5, yCoord-0.5, zCoord-0.5, xCoord+1.5, yCoord-0.5-range, zCoord+1.5))) {
+				if(e instanceof EntityPlayer)
+					continue;
+				e.motionX *= 0.8;
+				e.motionY -= 0.1;
+				e.motionZ *= 0.8;
+				e.fallDistance = 0;
+				e.setPosition(e.posX+(xCoord+0.5-e.posX)*0.1, e.posY, e.posZ+(zCoord+0.5-e.posZ)*0.1);
+			}
 		}
 	}
 	
