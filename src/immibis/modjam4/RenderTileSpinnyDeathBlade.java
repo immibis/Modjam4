7909ec89b1efcfdb0b4d3c5c828667980c7c9f73
immibis <immibis@gmail.com>
2014-05-18 15:34:39 +1200

    Automatic commit.

--- src/immibis/modjam4/RenderTileSpinnyDeathBlade.java
+++ src/immibis/modjam4/RenderTileSpinnyDeathBlade.java
@@ -15,29 +15,34 @@ public class RenderTileSpinnyDeathBlade extends RenderTileShaft {
 		IIcon i = Blocks.iron_block.getIcon(0, 0);
 		Tessellator t = Tessellator.instance;
 		
-		double r = 3.5;
+		double r = 4.5;
 		
-		double y1 = 0.5, y2 = 0.35;
+		double y1 = 0.45, y2 = 0.35;
 		
 		t.draw();
+		
+		GL11.glDisable(GL11.GL_CULL_FACE);
 		
 		for(int k = 0; k < 4; k++) {
 			GL11.glRotatef(90, 0, 1, 0);
 			
 			t.startDrawingQuads();
 			
-			double prevAngle = 0;
-			int numslices = 16;
+			double prevAngle = 0, prevDAngle = 0, prevradius = 0;
+			int numslices = 8;
 			for(int slice = 0; slice < numslices; slice++) {
-				double angle = 0;
-				double radius = (r + 1) * slice / numslices;
-				double prevradius = r * slice / numslices;
-				double dangle = 0.1;
-				t.addVertexWithUV(radius*Math.sin(angle), y2, radius*Math.sin(angle), i.getMaxU(), i.getMinV());
-				t.addVertexWithUV(radius*Math.sin(angle+dangle), y1, radius*Math.sin(angle+dangle), i.getMaxU(), i.getMaxV());
-				t.addVertexWithUV(w, y1,-w, i.getMinU(), i.getMaxV());
-				t.addVertexWithUV(w, y2, w, i.getMinU(), i.getMinV());
+				double radius = r * (slice + 1) / numslices + 0.125;
+				double angle = Math.atan(-0.25/radius) + (radius-0.25)/8 - 0.2;
+				double dangle = Math.PI / 4 / radius;// * (Math.sin(slice*Math.PI/2/numslices + Math.PI/2));
+				
+				t.addVertexWithUV(radius*Math.sin(angle), y1, radius*Math.cos(angle), i.getMaxU(), i.getMinV());
+				t.addVertexWithUV(radius*Math.sin(angle+dangle), y1, radius*Math.cos(angle+dangle), i.getMaxU(), i.getMaxV());
+				t.addVertexWithUV(prevradius*Math.sin(prevAngle+prevDAngle), y1, prevradius*Math.cos(prevAngle+prevDAngle), i.getMinU(), i.getMaxV());
+				t.addVertexWithUV(prevradius*Math.sin(prevAngle), y1, prevradius*Math.cos(prevAngle), i.getMinU(), i.getMinV());
+				
 				prevAngle = angle;
+				prevDAngle = dangle;
+				prevradius = radius;
 			}
 			
 			
@@ -47,6 +52,8 @@ public class RenderTileSpinnyDeathBlade extends RenderTileShaft {
 			//t.addVertexWithUV(m, y2, w+d, i.getMinU(), i.getMinV());
 			t.draw();
 		}
+		
+		GL11.glEnable(GL11.GL_CULL_FACE);
 		
 		t.startDrawingQuads();
 	}
--- src/immibis/modjam4/TileSpinnyDeathBlade.java
+++ src/immibis/modjam4/TileSpinnyDeathBlade.java
@@ -1,5 +1,13 @@
 package immibis.modjam4;
 
 public class TileSpinnyDeathBlade extends TileShaft {
-
+	@Override
+	public void updateEntity() {
+		super.updateEntity();
+		
+		if(!worldObj.isRemote) {
+			// 3 hearts/hit at 120 deg/s
+			double damage = shaftNode.getNetwork().angvel / (double)ShaftUtils.fromDegreesPerSecond(120);
+		}
+	}
 }
